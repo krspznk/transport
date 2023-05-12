aa46a6b8893f39e23eba5af830c94877e1d3ee8a
krspznk <106773100+krspznk@users.noreply.github.com>
2023-05-12 15:39:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,47 +1,3 @@
-#Метод для пошуку суми одномурного масиву
-def sum(array):
-    sum=0
-    for i in range(len(array)):
-        sum+=array[i]
-    return sum
-
-#Метод для пошуку мінімального елементу в масиві
-def min(array):
-    min=array[0]
-    for i in range(len(array)):
-        if array[i]<min:
-            min=array[i]
-    return min
-
-#Метод для пошуку індексу мінімального елементу масиву, який !=-1
-def minindex(array):
-    for i in range(len(array)):
-        if array[i]!=-1:
-            min=array[i]
-            ind=i
-            break
-    for i in range(len(array)):
-        n=array[i]
-        if (n<min) & (n!=-1):
-            min=n
-            ind=i
-    return ind
-
-#Метод для підрахунку суми матриці(для визначення загальної вартості перевезень)
-def sumarr(value, array2):
-    sum=0
-    for i in range(len(value)):
-        for j in range(len(value[i])):
-            if (array2[i][j]!=0)&(array2[i][j]!=-1):
-                sum+= value[i][j] * array2[i][j]
-    return sum
-
-#Метод для визначення типу задачі(відкритий/закритий), якщо задача відкритого типу, то виклиаємо додатковий метод vidkryta
-def perevirka(potreby, zapas):
-    if sum(potreby)!=sum(zapas):
-        vidkryta(sum(zapas)-sum(potreby), zapas)
-
-#Метод для транспонування матриці
 def trans(array):
     new = [[0 for i in range(len(array))] for i in range(len(array[0]))]
     for i in range(len(array)):
@@ -49,60 +5,133 @@ def trans(array):
             new[j][i] = array[i][j]
     return new
 
-#Якщо задача відкритого типу, то ми додаємо колонку в матрицю вартості та додаємо різницю потреб та запасів в потреби
-def vidkryta(num, zapas):
-    global value
-    new=[0]*len(zapas)
-    value.append(new)
-    global potreby
-    potreby.append(num)
+def sum(array):
+    s=0
+    for i in range(len(array)):
+        s+=array[i]
+    return s
 
-#За допомогою методу мінімальної вартості розставляємо значення перевезень відповідно до матриці вартостей
-def iteration(value, potreby, zapas):
-    perevirka(potreby, zapas)
-    value_copy=[]
+def sumar(value, vartist):
+    sumar=0
     for i in range(len(value)):
-        n=[]
         for j in range(len(value[i])):
-            n.append(value[i][j])
-        value_copy.append(n)
-    vartist=[[0 for _ in range(len(value_copy[0]))] for _ in range(len(value_copy))]
-    if sum(value_copy[-1]) == 0:
-        while sum(potreby[:-2])!=0:
-            for i in range(len(value_copy) - 1):
-                if potreby[i]!=0:
-                    ind=minindex(value_copy[i])
-                    if potreby[i]<zapas[ind]:
-                        zapas[ind]-=potreby[i]
-                        vartist[i][ind]=potreby[i]
-                        potreby[i]=0
-                    else:
-                        vartist[i][ind] =  zapas[ind]
-                        potreby[i]-=zapas[ind]
-                        zapas[ind]=0
-                    value_copy[i][ind]=-1
-        while sum(zapas)!=0:
-            for i in range(len(zapas)):
-                if zapas[i]!=0:
-                    vartist[-1][i] = zapas[i]
-                    potreby[-1] -= zapas[i]
-                    zapas[i] = 0
+            if value[i][j]>0:
+                sumar+=value[i][j]*vartist[i][j]
+    return sumar
 
-    else:
-        while sum(zapas) != 0:
-            for i in range(len(value_copy)):
-                if potreby[i]!=0:
-                    ind=minindex(value_copy[i])
-                    if potreby[i]<zapas[ind]:
-                        zapas[ind]-=potreby[i]
-                        vartist[i][ind]=potreby[i]
-                        potreby[i]=0
-                    else:
-                        vartist[i][ind] =  zapas[ind]
-                        potreby[i]-=zapas[ind]
-                        zapas[ind]=0
-                    value_copy[i][ind]=-1
-    return vartist
+def sumarray(value):
+    sumarray = 0
+    for i in range(len(value)):
+        for j in range(len(value[i])):
+            sumarray += value[i][j]
+    return sumarray
+
+
+def is_open(potreby, zapas):
+    num=0
+    if sum(potreby)!=sum(zapas):
+        num = sum(potreby)-sum(zapas)
+    return num
+
+def open_zapas(num, vartist, zapas):
+    for i in range(len(vartist)):
+        vartist[i].append(0)
+    zapas.append(num)
+    return (vartist, zapas)
+
+def open_potreby(num, vartist, potreby):
+    num=abs(num)
+    new = [0] * len(zapas)
+    vartist.append(new)
+    potreby.append(num)
+    return (vartist, potreby)
+
+
+
+def min_index(array):
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            if array[i][j]!=0:
+                min=array[i][j]
+                k=i
+                n=j
+                break
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            a=array[i][j]
+            if (array[i][j]<min) & (array[i][j]!=0):
+                min=array[i][j]
+                k=i
+                n=j
+    return (k, n)
+
+def min_vartist(vartist, potreby, zapas):
+    potreby_change = list(map(lambda x: x, potreby))
+    zapas_change = list(map(lambda x: x, zapas))
+    vartist_change = []
+    for i in range(len(vartist)):
+        n = []
+        for j in range(len(vartist[i])):
+            n.append(vartist[i][j])
+        vartist_change.append(n)
+    value = [[0 for _ in range(len(vartist_change[0]))] for _ in range(len(vartist_change))]
+    while (sum(potreby_change)-potreby_change[-1] != 0) & (sum(zapas_change)-zapas_change[-1] != 0):
+        i, j = min_index(vartist_change)
+        if potreby_change[i] > zapas_change[j]:
+            value[i][j] = zapas_change[j]
+            potreby_change[i] -= zapas_change[j]
+            zapas_change[j] = 0
+            vartist_change[i][j] = 0
+        elif potreby_change[i] < zapas_change[j]:
+            value[i][j] = potreby_change[i]
+            zapas_change[j] -= potreby_change[i]
+            potreby_change[i] = 0
+            vartist_change[i][j] = 0
+        else:
+            value[i][j] = potreby_change[i]
+            zapas_change[j] = 0
+            potreby_change[i] = 0
+            vartist_change[i][j] = 0
+    if (sum(potreby_change)-potreby_change[-1]==0) & (sum(zapas_change)!=0):
+        for i in range(len(vartist_change[-1])):
+            if potreby_change[-1] > zapas_change[i]:
+                value[-1][i] = zapas_change[i]
+                potreby_change[-1] -= zapas_change[i]
+                zapas_change[i] = 0
+                vartist_change[-1][i] = 0
+            elif potreby_change[-1] < zapas_change[i]:
+                value[-1][i] = potreby_change[-1]
+                zapas_change[i] -= potreby_change[-1]
+                potreby_change[-1] = 0
+                vartist_change[-1][i] = 0
+            else:
+                value[-1][i] = potreby_change[-1]
+                zapas_change[i] = 0
+                potreby_change[-1] = 0
+                vartist_change[-1][i] = 0
+            if (sum(potreby_change)==0) & (sum(zapas_change)==0):
+                break
+    elif (sum(zapas_change)-zapas_change[-1]==0) & (sum(potreby_change)!=0):
+        for i in range(len(vartist_change)):
+            if potreby_change[i] > zapas_change[-1]:
+                value[i][-1] = zapas_change[-1]
+                potreby_change[i] -= zapas_change[-1]
+                zapas_change[-1] = 0
+                vartist_change[i][-1] = 0
+            elif potreby_change[i] < zapas_change[-1]:
+                value[i][-1] = potreby_change[i]
+                zapas_change[-1] -= potreby_change[i]
+                potreby_change[i] = 0
+                vartist_change[i][-1] = 0
+            else:
+                value[i][-1] = potreby_change[i]
+                zapas_change[-1] = 0
+                potreby_change[i] = 0
+                vartist_change[i][-1] = 0
+            if (sum(potreby_change)==0) & (sum(zapas_change)==0):
+                break
+    return value
+
 
 #Перевірка матриці значень перевезень на виродженість
 def vyrodzhenist(value, potreby, zapas):
@@ -118,180 +147,147 @@ def vyrodzhenist(value, potreby, zapas):
 
 #Якщо матриця вироджена, то додаємо 'зайняту' клітинку і позначеємо як -1
 def vyrodarray(value, vartist):
-    while not vyrodzhenist(vartist, potreby, zapas):
-        min=value[0][0]
+    while not vyrodzhenist(value, potreby, zapas):
+        min=vartist[0][0]
         k=0
         n=0
         for i in range(len(value)):
             for j in range(len(value[i])):
-                if (vartist[i][j] == 0) & (value[i][j] < min) & (vartist[i][j] != -1) & (value[i][j] != 0):
+                if (value[i][j] == 0) & (vartist[i][j] < min) & (value[i][j] != -1) & (vartist[i][j] != 0):
                     min=vartist[i][j]
                     k=i
                     n=j
-        vartist[k][n]=-1
-    return vartist
+        value[k][n]=-1
+    return value
 
 #Вираховуэмо значення u, v для
 def find_u_v(value, vartist, zapas, potreby):
-    if vyrodzhenist(vartist, potreby, zapas):
-        u=[0 for i in range(len(zapas))]
-        v=[0 for i in range(len(potreby))]
-        uperevirka=[0]
-        vperevirka=[]
-        flag=len(potreby)+len(zapas)-1
-        value_t=trans(value)
-        vartist_t=trans(vartist)
-        for i in range(len(value_t[0])):
-            if ((vartist_t[0][i] != 0) | (vartist_t[0][i] == -1))& (i not in vperevirka):
-                v[i]=value_t[0][i]
-                vperevirka.append(i)
+    u = [0 for i in range(len(zapas))]
+    v = [0 for i in range(len(potreby))]
+    uperevirka = [0]
+    vperevirka = []
+    flag = len(potreby) + len(zapas) - 1
+    vartist_t = trans(vartist)
+    if not vyrodzhenist(value, potreby, zapas):
+        value=vyrodarray(value, vartist)
+    value_t = trans(value)
+    for i in range(len(value_t[0])):
+        if ((value_t[0][i] != 0) | (value_t[0][i] == -1))& (i not in vperevirka):
+            v[i]=vartist_t[0][i]
+            vperevirka.append(i)
+            flag-=1
+    i=1
+    while flag!=0:
+        for j in vperevirka:
+            if ((value_t[i][j] != 0) | (value_t[i][j] == -1)) & (i not in uperevirka):
+                u[i]= vartist_t[i][j] - v[j]
                 flag-=1
-        i=1
-        while flag!=0:
-            for j in vperevirka:
-                if ((vartist_t[i][j] != 0) | (vartist_t[i][j] == -1)) & (i not in uperevirka):
-                    u[i]= value_t[i][j] - v[j]
-                    flag-=1
-                    uperevirka.append(i)
-            for j in range(len(value_t[i])):
-                if ((vartist_t[i][j] != 0) | (vartist_t[i][j] == -1))  & (j not in vperevirka):
-                    v[j] = value_t[i][j] - u[i]
-                    vperevirka.append(j)
-                    flag -= 1
-            i+=1
-    else:
-        vyrodarray(value, vartist)
-        u = [0 for i in range(len(zapas))]
-        v = [0 for i in range(len(potreby))]
-        uperevirka = [0]
-        vperevirka = []
-        flag = len(potreby) + len(zapas) - 1
-        value_t = trans(value)
-        vartist_t = trans(vartist)
-        for i in range(len(value_t[0])):
-            if ((vartist_t[0][i] != 0) | (vartist_t[0][i] == -1)) & (i not in vperevirka):
-                v[i] = value_t[0][i]
-                vperevirka.append(i)
+                uperevirka.append(i)
+        for j in range(len(value_t[i])):
+            if ((value_t[i][j] != 0) | (value_t[i][j] == -1))  & (j not in vperevirka):
+                v[j] = vartist_t[i][j] - u[i]
+                vperevirka.append(j)
                 flag -= 1
-        i = 1
-        while flag != 0:
-            for j in vperevirka:
-                if ((vartist_t[i][j] != 0) | (vartist_t[i][j] == -1)) & (i not in uperevirka):
-                    u[i] = value_t[i][j] - v[j]
-                    flag -= 1
-                    uperevirka.append(i)
-            for j in range(len(value_t[i])):
-                if ((vartist_t[i][j] != 0) | (vartist_t[i][j] == -1)) & (j not in vperevirka):
-                    v[j] = value_t[i][j] - u[i]
-                    vperevirka.append(j)
-                    flag -= 1
-            i += 1
+        i+=1
+        if i>=len(zapas):
+            i=0
+
     return (u, v)
 
 #Перевіряємо оптимальність плану методом потенціалів, якщо план не оптимальний, переходимо до функції pattern
-def pererahunok(value, vartist, u, v):
+def potentials(value, vartist, u, v):
     value_t=trans(value)
     vartist_t=trans(vartist)
+    potential=[[0 for i in range(len(vartist))] for i in range(len(vartist[0]))]
     flag=0
     for i in range(len(value_t)):
         for j in range(len(value_t[i])):
-            if vartist_t[i][j]==0:
-                vartist_t[i][j]= (u[i] + v[j]) - value_t[i][j]
-                if vartist_t[i][j]>0:
-                    flag=vartist_t[i][j]
-            elif vartist_t[i][j]==-1:
-                vartist_t[i][j]=0
-    return (trans(vartist_t), flag)
+            if value_t[i][j]==0:
+                potential[i][j]= (u[i] + v[j]) - vartist_t[i][j]
+                if potential[i][j]>0:
+                    flag=potential[i][j]
+            elif value_t[i][j]==-1:
+                value_t[i][j]=0
+    return (trans(potential), flag)
 
 
-#За допомогою перетворень шукаємо оптимальний план(для кожного нового плану виконуємо методи find_u_v та potentsial)
-def pattern(value, vartist, vartist_main, zapas, potreby, flag):
-    array2t=trans(vartist)
-    no=[]
-    no_t=[]
-    k=0
-    n=0
-    a1=0
-    a2=0
-    while flag!=0:
-        for i in range(len(array2t)):
-            for j in range(len(array2t[i])):
-                if array2t[i][j]==flag:
-                    k=i
-                    n=j
-        for i in range(len(vartist[n])):
-            if (vartist[n][i] > 0) & (vartist[n][i] != flag) & (vartist[n][i] not in no):
-                a1=i
-                break
-        for i in range(len(array2t[k])):
-            if (array2t[k][i]>0) & (array2t[k][i]!=flag) & (array2t[k][i] not in no_t):
-                a2=i
-                break
-        while vartist_main[a2][a1] <=0:
-            no.append(vartist_main[n][a1])
-            no_t.append(vartist_main[a2][k])
-            for i in range(len(vartist_main[n])):
-                if (vartist_main[n][i] > 0) & (vartist_main[n][i] not in no):
-                    a1 = i
-                    break
-            if vartist_main[a1][a2] >0:
-                break
-            for i in range(len(vartist_main)):
-                if (vartist_main[i][k] > 0)  & (vartist_main[i][k] not in no_t):
-                    a2 = i
-                    break
-
-        num=min([vartist_main[a2][k], vartist_main[n][a1]])
-        vartist_main[n][k]=num
-        vartist_main[n][a1]-=num
-        vartist_main[a2][k]-=num
-        vartist_main[a2][a1]+=num
-        u, v = find_u_v(value, vartist_main, zapas, potreby)
-        vartist, flag = pererahunok(value, vartist, u, v)
-    return vartist_main
-
-#Метод для виводу фінальних значень
-def show(array1, array2, n):
-    print("Vartist` perevezen`: " + str(sumarr(array1, array2)))
-    print()
-    print("Perevezenya:")
-    for i in range(len(array2[:n])):
-        print(("B"+str(i+1)).rjust(6).ljust(4), end="")
-    print()
-
-    array2=trans(array2)
-    for i in range(len(array2)):
-        print(("A" + str(i + 1)), end="  ")
-        for j in range(len(array2[i][:n])):
-            if (array2[i][j] == 0) | (array2[i][j] == -1):
-                print(str(0).rjust(1).ljust(5), end="")
-            else:
-                print(str(array2[i][j]).rjust(2).ljust(6), end="")
-        print()
+def pererahunok(potential, value, flag):
+    for i in range(len(value)):
+        for j in range(len(value[i])):
+            if potential[i][j]>0:
+                col=i
+                row=j
+    rows=[]
+    cols=[]
+    for i in range(len(value[0])):
+        a=value[col][i]
+        if value[col][i]>0:
+            rows.append(i)
+    for i in range(len(value)):
+        b=value[i][row]
+        if value[i][row]>0:
+            cols.append(i)
+    c=-1
+    for i in cols:
+        for j in rows:
+            l=value[i][j]
+            if value[i][j]>0:
+                c=i
+                r=j
+    if c==-1:
+        array=[[0 for i in range(len(value[i]))] for i in range(len(value))]
+        return array
+    n=min(value[c][row], value[col][r])
+    value[c][row]-=n
+    value[col][r]-=n
+    value[col][row]=n
+    value[c][r]+=n
+    return value
 
 
-#Стручктуруючий метод
-def final(value, potreby, zapas):
+def final(vartist, potreby, zapas):
     n = len(potreby)
-    vartist = iteration(value, potreby, zapas)
+    k = len(zapas)
+    num = is_open(potreby, zapas)
+    if num > 0:
+        vartist, zapas = open_zapas(num, vartist, zapas)
+    elif num < 0:
+        vartist, potreby = open_potreby(num, vartist, potreby)
+    value = min_vartist(vartist, potreby, zapas)
     u, v = find_u_v(value, vartist, zapas, potreby)
-    vartist_main, flag = pererahunok(value, vartist, u, v)
-    if flag == 0:
-        show(value, vartist, n)
-    else:
-        vartist = pattern(value, vartist_main, vartist, zapas, potreby, flag)
-        show(value, vartist, n)
+    potential, flag = potentials(value, vartist, u, v)
+    while flag != 0:
+        value_new = pererahunok(potential, value, flag)
+        if sumarray(value_new) == 0:
+            break
+        else:
+            value = value_new
+        u, v = find_u_v(value, vartist, zapas, potreby)
+        potential, flag = potentials(value, vartist, u, v)
+    for i in range(len(value)):
+        for j in range(len(value[i])):
+            if value[i][j] == -1:
+                value[i][j] = 0
+    value_t = trans(value)
+    for i in range(k):
+        for j in range(n):
+            print(value_t[i][j], end="   ")
+        print()
+    print("Sum: ", sumar(value, vartist))
 
-# value=[[2, 3, 4], [5, 1, 2], [3, 2, 1], [1, 4, 5]]
-# potreby=[40, 50, 110, 40]
-# zapas=[90, 80, 70]
-# value=[[4, 5], [6, 5], [3, 2]]
+vartist=[[2, 3, 4], [5, 1, 2], [3, 2, 1], [1, 4, 5]]
+potreby=[40, 50, 110, 40]
+zapas=[90, 80, 70]
+# vartist=[[1, 2, 3], [3, 1, 5], [2, 4, 6], [4, 3, 1]]
+# potreby=[30, 10, 20, 40]
+# zapas=[35, 50, 15]
+# vartist=[[4, 5], [6, 5], [3, 2]]
 # potreby=[1000, 1300, 1200]
 # zapas=[2000, 2500]
-
-value=[[1, 2, 3], [3, 1, 5], [2, 4, 6], [4, 3, 1]]
-potreby=[30, 10, 20, 40]
-zapas=[35, 50, 15]
-
-final(value, potreby, zapas)
-
+# vartist=[[7, 4, 9], [8, 5, 2], [1, 9, 3], [2, 8, 6]]
+# potreby=[120, 50, 190, 110]
+# zapas=[160, 140, 170]
+# vartist=[[6, 8, 5, 9], [4, 5, 6, 7 ], [7, 9, 8, 4]]
+# potreby=[120, 100, 110]
+# zapas=[105, 55, 90, 80]
+final(vartist, potreby, zapas)
